0d47a1db5ac352a2c7095bfe0b6b150cd9fee489
Ahmedyahyay <ahmed@example.com>
2025-03-26 04:27:22 +0300

    feat: Refactor _printf with helper functions

--- _printf.c
+++ _printf.c
@@ -1,46 +1,52 @@
 #include "main.h"
 /**
- * _printf - A custom implementation of the printf function
- * @format: The format string containing the characters and specifiers
- * Return: The number of characters printed (excluding the null byte)
+ *_printf - Custom printf implementation.
+ * @format: The format string.
+ * @...: Variable arguments based on the format string.
+ *
+ * Description: This function mimics the standard printf function. It processes
+ * the format string, handling format specifiers (%c, %s, %d, %i, %%),
+ * and writes the corresponding output. It relies on helper functions
+ * (write_char, write_str, write_int) to perform the actual writing.
+ *
+ * Return: The number of characters printed (excluding the null byte).
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0;
+	va_list arg;
+	int len = 0, i = 0;
+	char crntchar, *crntstring;
 
-	va_start(args, format);
-
-	while (*format)
+	va_start(arg, format);
+	while (format[i])
 	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == 'c')
+		if (format[i] == '%')
+			switch (format[i + 1])
 			{
-				char c = va_arg(args, int);
-
-				count += write(1, &c, 1);
-			}
-			else if (*format == 's')
-			{
-				char *s = va_arg(args, char *);
-
-				count += s ? write(1, s, strlen(s)) : write(1, "(null)", 6);
-			}
-			else if (*format == '%')
-			count += write(1, "%", 1);
-			else
-			{
-				count += write(1, "%", 1);
-				count += write(1, format, 1);
-			}
+				case 'c':
+					crntchar = (char)va_arg(arg, int);
+					write_char(&len, crntchar), i += 2;
+					break;
+				case 's':
+					crntstring = va_arg(arg, char*);
+					write_str(&len, crntstring), i += 2;
+					break;
+				case 'd':
+				case 'i':
+					write_int(&len, va_arg(arg, int)), i += 2;
+					break;
+				case '%':
+					write_char(&len, '%'), i += 2;
+					break;
+				case '\0':
+					continue;
+				default:
+					write_char(&len, format[i]), i++;
+					break;
 			}
 		else
-			count += write(1, format, 1);
-			format++;
+			write_char(&len, format[i]), i++;
 	}
-
-	va_end(args);
-	return (count);
+	va_end(arg);
+	return (len);
 }
--- main.h
+++ main.h
@@ -1,11 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdarg.h>
 #include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
 
 int _printf(const char *format, ...);
-
+void write_char(int *len, char c);
+void write_str(int *len, char *str);
+void write_int(int *len, int num);
 #endif
